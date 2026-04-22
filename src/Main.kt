f022cbb5f57d693ef3ca48256392f72077d93e9c
waimea-zbulley <zgbulley@waimea.school.nz>
2026-04-23 11:47:31 +1200

    Properly integrated classes

--- src/Main.kt
+++ src/Main.kt
@@ -17,6 +17,13 @@ val boxes = mutableListOf<String>()
 const val player1Colour = "#FFFF00"
 const val player2Colour = "#D60BA8"
 
+data class Player(
+    val token: String,
+    var score: Int,
+    val name: String
+)
+
+
 fun main() {
     println("Welcome to Chain Reaction!")
     println("--------------------------")
@@ -81,19 +88,10 @@ fun showRules() {
 fun game() {
     val player1Token = "X"
     val player2Token = "O"
-
-    data class Player(
-        val token: String,
-        var score: Int,
-        val name: String
-    )
-
     val player1 = Player(name = playerNames("Player 1"), token = player1Token, score = 0)
     val player2 = Player(name = playerNames("Player 2"), token = player2Token, score = 0)
 
     var otherPlayer = player2
-    var otherPlayerToken = player2Token
-
     var currentPlayer = player1
 
     createBoxes()
@@ -105,13 +103,13 @@ fun game() {
 
     println("")
 
-    showBoxes(player1Token, player2Token)
+    showBoxes(player1, player2)
     while (player1.score < winScore && player2.score < winScore) {
         var scoreAdd = 0
         // Code for player turn
-        playTurn(currentPlayer.name, currentPlayer.token, otherPlayerToken, player1Token, player2Token)
-        checkBoxesForPushToken(currentPlayer.token, otherPlayerToken, currentPlayer.name, otherPlayer.name)
-        scoreAdd += checkBoxesForChain(currentPlayer.token)
+        playTurn(currentPlayer, otherPlayer, player1, player2)
+        checkBoxesForPushToken(currentPlayer, otherPlayer)
+        scoreAdd += checkBoxesForChain(currentPlayer)
 
 
         // Code to switch player
@@ -120,18 +118,16 @@ fun game() {
 
             currentPlayer = player2
             otherPlayer = player1
-            otherPlayerToken = "X"
         } else {
             currentPlayer.score += scoreAdd
 
             currentPlayer = player1
             otherPlayer = player2
-            otherPlayerToken = "O"
         }
 
 
-        showScore(player1.name, player2.name, player1.score, player2.score)
-        showBoxes(player1Token, player2Token)
+        showScore(player1, player2)
+        showBoxes(player1, player2)
 
 
         checkForPlayerWin(currentPlayer = player1.name, currentPlayerScore = player1.score)
@@ -151,7 +147,7 @@ fun createBoxes() {
     }
 }
 
-fun showBoxes(player1Token: String, player2Token: String) {
+fun showBoxes(player1: Player, player2: Player) {
 
     // Returns if list is empty in order to not print only ending and starting segments of boxes
     if (boxes.isEmpty()) return
@@ -167,8 +163,8 @@ fun showBoxes(player1Token: String, player2Token: String) {
     for (i in boxes) {
         print("│")
         when (i) {
-            player1Token -> print(" $i ".col(hex = player1Colour))
-            player2Token -> print(" $i ".col(hex = player2Colour))
+            player1.token -> print(" $i ".col(hex = player1Colour))
+            player2.token -> print(" $i ".col(hex = player2Colour))
             else -> print(" $i ".padEnd(3))
         }
     }
@@ -204,21 +200,20 @@ fun playerNames(currentPlayer: String): String {
 }
 
 fun playTurn(
-    currentPlayer: String,
-    currentPlayerToken: String,
-    otherPlayerToken: String,
-    player1Token: String,
-    player2Token: String
+    currentPlayer: Player,
+    otherPlayer: Player,
+    player1: Player,
+    player2: Player
 ) {
 
     while (true) {
-        when (currentPlayerToken) {
-            player1Token -> print(
-                "$currentPlayer's".col(hex = player1Colour) + " turn please select what square you would like to place your token in (1-${boxes.size}): "
+        when (currentPlayer.token) {
+            player1.token -> print(
+                "${currentPlayer.name}'s".col(hex = player1Colour) + " turn please select what square you would like to place your token in (1-${boxes.size}): "
             )
 
-            player2Token -> print(
-                "$currentPlayer's".col(hex = player2Colour) + " turn please select what square you would like to place your token in (1-${boxes.size}): "
+            player2.token -> print(
+                "${currentPlayer.name}'s".col(hex = player2Colour) + " turn please select what square you would like to place your token in (1-${boxes.size}): "
             )
         }
         val turn = readln().toIntOrNull()
@@ -228,12 +223,12 @@ fun playTurn(
             turn > numBoxes -> println("Position does not exist (value too high)".red())
             turn < 1 -> println("Position does not exist (value too low)".red())
             // Need to check that...
-            turn in 2..<boxes.size - 1 && boxes[turn] == otherPlayerToken && boxes[turn - 2] == otherPlayerToken -> println(
+            turn in 2..<boxes.size - 1 && boxes[turn] == otherPlayer.token && boxes[turn - 2] == otherPlayer.token -> println(
                 "Invalid Spot".red()
             )
             // Only make move if blank
             boxes[turn - 1] == "-" -> {
-                boxes[turn - 1] = currentPlayerToken
+                boxes[turn - 1] = currentPlayer.token
                 break
             }
 
@@ -243,20 +238,18 @@ fun playTurn(
 }
 
 fun checkBoxesForPushToken(
-    currentPlayerToken: String,
-    otherPlayerToken: String,
-    currentPlayer: String,
-    otherPlayer: String
+    currentPlayer: Player,
+    otherPlayer: Player
 ) {
     for (i in 1..<boxes.size - 1) {
-        if (boxes[i] == otherPlayerToken && boxes[i - 1] == currentPlayerToken && boxes[i + 1] == currentPlayerToken) {
-            println("$currentPlayer pushes out one of $otherPlayer's tokens")
+        if (boxes[i] == otherPlayer.token && boxes[i - 1] == currentPlayer.token && boxes[i + 1] == currentPlayer.token) {
+            println("${currentPlayer.name} pushes out one of ${otherPlayer.name}'s tokens")
             boxes[i] = "-"
         }
     }
 }
 
-fun checkBoxesForChain(playerToken: String): Int {
+fun checkBoxesForChain(currentPlayer: Player): Int {
     var playerScoreAdd = 0
     var count = 0
     val minChainLeng = 3
@@ -284,17 +277,17 @@ fun checkBoxesForChain(playerToken: String): Int {
     return playerScoreAdd
 }
 
-fun showScore(player1: String, player2: String, player1Score: Int, player2Score: Int) {
+fun showScore(player1: Player, player2: Player) {
     println("Scores")
     print("┌────────────────────┐")
     println("┌────────────────────┐")
 
     print("│")
-    print(" $player1 : ${player1Score.toString().padEnd(16 - player1.length).col(hex = player1Colour)}")
+    print(" ${player1.name} : ${player1.score.toString().padEnd(16 - player1.name.length).col(hex = player1Colour)}")
     print("│")
 
     print("│")
-    print(" $player2 : ${player2Score.toString().padEnd(16 - player2.length).col(hex = player2Colour)}")
+    print(" ${player2.name} : ${player2.score.toString().padEnd(16 - player2.name.length).col(hex = player2Colour)}")
     println("│")
     print("└────────────────────┘")
     println("└────────────────────┘")
